3159e3646259eae5330fc23c0e467cd036d16f90
David Li <li.davidm96@gmail.com>
2016-05-17 11:11:01 -0400

    feat: add editable title to note

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
 import xs from "xstream";
 import debounce from "xstream/extra/debounce";
 import * as dom from "@cycle/dom";
+import isolate from "@cycle/isolate";
 
 import md from "markdown-it";
 import mdTaskLists from "markdown-it-task-lists";
 
 const parser = md().use(mdTaskLists);
+
+function EditableTitle(sources) {
+    const initialValue$ = sources.props$.map(props => props.initial);
+    const newValue$ = sources.DOM.select("h1").events("input").map(ev => ev.target.textContent);
+
+    const value$ = initialValue$.merge(newValue$).remember();
+
+    const view$ = xs.combine((title) => {
+        return dom.h1({ props: { contentEditable: true } }, title);
+    }, value$);
+
+    const sinks = {
+        DOM: view$,
+        value$,
+    };
+
+    return sinks;
+}
 
 function intent(DOM) {
     return {
@@ -21,20 +40,30 @@ function model(actions) {
     };
 }
 
-function view(state) {
-    return state.currentDocument$.map(markdown => dom.div([
-        dom.textarea(),
-        dom.div({
-            props: { innerHTML: parser.render(markdown) }
-        }),
-    ]));
+function view(noteTitle, state) {
+    return xs.combine((titleVTree, markdown, title) => {
+        return dom.div([
+            dom.textarea(),
+            titleVTree,
+            dom.div({
+                props: { innerHTML: parser.render(markdown) }
+            }),
+        ]);
+    }, noteTitle.DOM, state.currentDocument$, noteTitle.value$);
 }
 
 export default function app(sources) {
+    const noteTitle = isolate(EditableTitle)({
+        DOM: sources.DOM,
+        props$: xs.of({
+            initial: "New Note",
+        }),
+    });
+
     const actions = intent(sources.DOM);
     const state$ = model(actions);
     const sinks = {
-        DOM: view(state$),
+        DOM: view(noteTitle, state$),
     };
 
     return sinks;
